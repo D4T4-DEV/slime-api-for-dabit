97dc9a7fb7d1e2d99131b3a18995b495dbf9789a
David Trejo <d4t4_dev@hotmail.com>
2026-06-30 22:15:15 -0600

    Update server.ts
    Corrección de localhost en formato IPv4

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import { SERVER_PORT } from "./core/constants/app.constants.js";
 
 app.listen(SERVER_PORT, () => {
     // El servidor escuchara en localhost en el puerto especificado
-    console.log(`Server listening in http://127.0.0.0:${SERVER_PORT}`)
+    console.log(`Server listening in http://127.0.0.1:${SERVER_PORT}`)
 });
