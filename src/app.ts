02551d7923de2205ed1d47e20bba9518fe994b13
David Trejo <d4t4_dev@hotmail.com>
2026-06-30 22:24:24 -0600

    Update app.ts
    Usamos un router por escalabilidad de la app y mantenibilidad

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,13 @@
-import express, { type Express, type Request, type Response } from 'express'
+import express, { type Express } from 'express'
 import "dotenv/config" // - Importar variables de entorno definidas en el archivo .env
+import appRouter from './routes/router.js';
 
 const app: Express = express();
 
 // Aceptar body en formato JSON 
 app.use(express.json());
 
-app.get("/", (_req: Request, res: Response) => {
-    res.status(200).json({
-        status: 200,
-        message: "¡Hola a mi API de Slimes!"
-    });
-});
+// Rutas de la API
+app.use("/", appRouter);
 
 export default app;
