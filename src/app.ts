d481bab9f1fbb282ea769408488afd7bd6b90070
David Trejo <d4t4_dev@hotmail.com>
2026-06-30 22:15:36 -0600

    Update app.ts
    Hacemos el endpoint principal de la api para saludar

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
-import express, { type Express } from 'express'
+import express, { type Express, type Request, type Response } from 'express'
 import "dotenv/config" // - Importar variables de entorno definidas en el archivo .env
 
 const app: Express = express();
 
 // Aceptar body en formato JSON 
 app.use(express.json());
+
+app.get("/", (_req: Request, res: Response) => {
+    res.status(200).json({
+        status: 200,
+        message: "¡Hola a mi API de Slimes!"
+    });
+});
 
 export default app;
