d2a45db5b5f877bd191563185a86558702d18ebb
David Trejo <d4t4_dev@hotmail.com>
2026-07-01 20:23:05 -0600

    Update app.ts
    Uso de la libreria para manejar CORS

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { type Express } from 'express'
+import cors from "cors";
 import "dotenv/config" // - Importar variables de entorno definidas en el archivo .env
 import appRouter from './routes/router.js';
 
@@ -6,6 +7,9 @@ const app: Express = express();
 
 // Aceptar body en formato JSON 
 app.use(express.json());
+
+// Habilitar CORS (por sus siglas en inglés, Cross-Origin Resource Sharing)
+app.use(cors());
 
 // Rutas de la API
 app.use("/", appRouter);
