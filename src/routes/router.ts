fb619cc7a0f5d7784190947b2955607938a8d62a
David Trejo <d4t4_dev@hotmail.com>
2026-07-01 22:02:56 -0600

    Acciones de la api para el endpoint de slimes

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,4 +1,5 @@
 import express, { type Router, type Request, type Response } from 'express'
+import slimeRouter from './slime.routes.js';
 
 const appRouter: Router = express.Router();
 
@@ -9,12 +10,7 @@ appRouter.get("/", (_req: Request, res: Response) => {
     });
 });
 
-appRouter.get("/slime", (_req: Request, res: Response) => {
-    res.status(200).json({
-        status: 200,
-        message: "¡Hola endpoint de Slimes"
-    });
-});
+appRouter.use("/slime", slimeRouter);
 
 appRouter.get("/auth", (_req: Request, res: Response) => {
     res.status(200).json({
